3348016fd384bb5852a1542a940aeb6e2371bae2
river <banh1809@gmail.com>
2018-09-15 22:38:49 -0700

    edit comment

--- application/app_button_control.h
+++ application/app_button_control.h
@@ -5,7 +5,12 @@
 #include <stdbool.h>
 #include "tm_stm32f4_disco.h"
 
+/* brief: init button board
+*/
 void app_button_init(void);
+
+/* brief: check status button board
+*/
 bool app_button_read_state(void);
 
 #endif
