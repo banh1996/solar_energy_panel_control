1386509adfc7d36b35a3857c645f5d94b4e06194
hung <banh1809@gmail.com>
2018-12-07 10:53:17 +0700

    ok

--- Targets/Nucleo_F411_ext_8MHz/app_lcd_task.d
+++
@@ -1,5 +0,0 @@
-.\targets\nucleo_f411_ext_8mhz\app_lcd_task.o: application\app_lcd_task.c
-.\targets\nucleo_f411_ext_8mhz\app_lcd_task.o: application\app_lcd_task.h
-.\targets\nucleo_f411_ext_8mhz\app_lcd_task.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\targets\nucleo_f411_ext_8mhz\app_lcd_task.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\targets\nucleo_f411_ext_8mhz\app_lcd_task.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
